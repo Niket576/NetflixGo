034b776ef44b127d99aa889fb12867ed8f7f0101
Niket576 <niketchaudhary576@gmail.com>
2025-04-26 21:48:36 +0530

    production ready application

--- backend/index.js
+++ backend/index.js
@@ -7,26 +7,37 @@ import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoute.js";
 import cors from "cors";
 
+import path from "path";
+
 databaseConnection();
 
 dotenv.config({
-    path:".env"
+    path: ".env"
 })
 
 const app = express();
+
+const _dirname = path.resolve();
+
+
 //middlewares 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:3000',
-    credentials:true
+    origin: 'http://localhost:3000',
+    credentials: true
 }
 app.use(cors(corsOptions));
- 
+
 // api
 app.use("/api/v1/user", userRoute);
 
-app.listen(process.env.PORT,() => {
+app.use(express.static(path.join(_dirname, "/frontend/build")));
+app.get("*", (_, res) => {
+    res.sendFile(path.join(_dirname, "/frontend/build/index.html"));
+});
+
+app.listen(process.env.PORT, () => {
     console.log(`Server listen at port ${process.env.PORT}`);
 });
